54b532dddb2916619f088445d9902b85faf46240
Murray Sinclair <msin981@gmail.com>
2023-11-16 16:38:15 +1300

    updated endpoint

--- client/apis/index.ts
+++ client/apis/index.ts
@@ -4,6 +4,6 @@ const SSURL = 'http://localhost:3000'
 
 // THIS ENDPOINT WILL CHANGE ONCE TAYNE AND BONNIE FINISH SERVERSIDE ROUTING
 async function getTodos():Promise<Todo> {
-  const data = await request.get(SSURL + '/api/todos') 
+  const data = await request.get(SSURL + '/api/v1/todos') 
   return data.body
 }
